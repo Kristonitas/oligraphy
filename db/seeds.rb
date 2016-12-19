bc28bac312c6f7024ce4642d59c05024c1c4c549
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2016-12-19 20:53:02 +0200

    Article, Markdown added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Article.create(title: 'Redakcijos žodis', content: 'Kadangi niekas kitas _atseit_ *negali* **padaryti**')
